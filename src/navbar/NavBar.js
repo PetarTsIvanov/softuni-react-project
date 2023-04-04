b2647092a3205e39ba9999eb0b0785ffeca5de2b
Petar <104589834+PetarTsIvanov@users.noreply.github.com>
2023-04-04 23:58:01 +0300

    Woooo finally doing this, about time

---
+++ src/navbar/NavBar.js
@@ -0,0 +1,12 @@
+import "./NavBar.css"
+
+export default function NavBar() {
+    return (
+        <div className="top">
+            <div className="topLeft">
+            </div>
+            <div className="topCenter">C</div>
+            <div className="topRight">R</div>
+        </div>
+    )
+}
