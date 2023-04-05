07242f2b5bec954e28dd75d6e591e174796883ca
Petar <104589834+PetarTsIvanov@users.noreply.github.com>
2023-04-05 23:05:15 +0300

    Added header

---
+++ src/header/Header.js
@@ -0,0 +1,17 @@
+import './Header.css';
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="headerTitles">
+                <span className="headerTitleSm">React & Node</span>
+                <span className="headerTitleLg">Blog</span>
+            </div>
+            <img
+                className="headerImg"
+                src="https://images.unsplash.com/photo-1680639224103-133fad8b5763?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
+                alt=""
+            />
+        </div>
+    )
+}
