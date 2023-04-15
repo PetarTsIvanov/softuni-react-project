56dd86c820c744535b34755eaf2dab3fa9cbfba5
Petar <104589834+PetarTsIvanov@users.noreply.github.com>
2023-04-15 14:07:14 +0300

    Added routing

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom"
 import "./Navbar.css"
 
 export default function Navbar() {
+    const user = false;
     return (
         <div className="top">
             <div className="topLeft">
@@ -11,19 +13,41 @@ export default function Navbar() {
             </div>
             <div className="topCenter">
                 <ul className="topList">
-                    <li className="topListItem">HOME</li>
-                    <li className="topListItem">ABOUT</li>
-                    <li className="topListItem">CONTACT</li>
-                    <li className="topListItem">WRITE</li>
-                    <li className="topListItem">LOGOUT</li>
+                    <li className="topListItem">
+                        <Link className="link" to={"/"}>HOME</Link>
+                    </li>
+                    <li className="topListItem">
+                        <Link className="link" to={"/"}>ABOUT</Link>
+                    </li>
+                    <li className="topListItem">
+                        <Link className="link" to={"/"}>CONTACT</Link>
+                    </li>
+                    <li className="topListItem">
+                        <Link className="link" to={"/write"}>WRITE</Link>
+                    </li>
+                    <li className="topListItem">
+                        {user && "LOGOUT"}
+                    </li>
                 </ul>
             </div>
             <div className="topRight">
-                <img
-                    className="topImg"
-                    src="https://images.unsplash.com/photo-1680006022769-fdc2605a3dd4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-                    alt=""
-                />
+                {user ? (
+                    <img
+                        className="topImg"
+                        src="https://images.unsplash.com/photo-1680006022769-fdc2605a3dd4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+                        alt=""
+                    />
+                ) : (
+                    <ul className="topList">
+                        <li className="topListItem">
+                            <Link className="link" to="/login">LOGIN</Link>
+                        </li>
+                        <li className="topListItem">
+                            <Link className="link" to="/register">REGISTER</Link>
+                        </li>
+                    </ul>
+                )}
+
                 <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
             </div>
         </div>
